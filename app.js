4dd8c2643f9c06516ff46ea409dedb880b8da6c3
KhylaRedd <kreddick@pursuit.org>
2024-07-03 12:58:17 -0400

    Update attempts to fix error

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 const express = require('express');
 const app = express();
 const budgetController = require('./controller/budgetController');
+const cors = require('cors');
+const budget = require('./controller/budgetController');
+require('dotenv').config();
 
 app.use(express.json());
+app.use(cors());
 
+//routes
 app.use('/Budget', budgetController);
 
-
 app.get('/', (req, res)=>{
-    res.send ("Budget Breakdown");
+    res.send ({budget});
 });
+
+
 
 
 module.exports = app;
